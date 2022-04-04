349c17d9bfa5066b3fe131388d7d52028c80b22a
Martina <palmucci997@gmail.com>
2022-04-04 12:41:03 +0200

    test commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 use curve25519_dalek_ng::scalar::Scalar;
 use rand_core::OsRng;
 
+//test comment
+
+fn main() {
+    
+    let mut csprng = OsRng;
+
+    let x = Scalar::zero();
+    let mut points = Vec::<(Scalar, Scalar)>::new();
+    for _ in 0..10 {
+        points.push((Scalar::random(&mut csprng), Scalar::random(&mut csprng)));
+    }
+    let index_j = 2;
+
+    let lagrange_pol = compute_interpolation_lagrange(x, &points);
+
+    println!("{:?}", lagrange_pol);
+}
+
+
+
 // algorithm
 fn compute_interpolation_lagrange(x: Scalar, points: &[(Scalar, Scalar)]) -> Scalar {
 
@@ -27,21 +47,4 @@ fn compute_lagrange_polinomial(j: usize, points: &[(Scalar, Scalar)], x: Scalar)
     }
 
     l_j
-}
-
-
-fn main() {
-    
-    let mut csprng = OsRng;
-
-    let x = Scalar::zero();
-    let mut points = Vec::<(Scalar, Scalar)>::new();
-    for _ in 0..10 {
-        points.push((Scalar::random(&mut csprng), Scalar::random(&mut csprng)));
-    }
-    let index_j = 2;
-
-    let lagrange_pol = compute_interpolation_lagrange(x, &points);
-
-    println!("{:?}", lagrange_pol);
 }
