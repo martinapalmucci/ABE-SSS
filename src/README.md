9290ef82434c428829ea4da54c92871c2a25f113
Martina <palmucci997@gmail.com>
2022-04-04 16:31:58 +0200

    lagrange interpolation test

---
+++ src/README.md
@@ -0,0 +1,2 @@
+# READ ME for ABE-SSS
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,55 @@
 // mod main;
+use curve25519_dalek_ng::scalar::Scalar;
 
-fn lagrange_interpolate(){
+pub fn lagrange_interpolate(x: Scalar, points: &[(Scalar, Scalar)]) -> Scalar {
+    let mut y: Scalar = Scalar::zero();
 
+    for (j, (_, y_j)) in points.iter().enumerate() {
+        let l_j = lagrange_polynomial(j, points, x);
+        y += y_j * l_j;
+    }
 
-
-    //(x - x_m)/(x_j - x_m)
-
+    y
 }
 
-fn lagrange_polinomial(){
+fn lagrange_polynomial(j: usize, points: &[(Scalar, Scalar)], x: Scalar) -> Scalar {
+    let mut l_j: Scalar = Scalar::one();
 
-    //x.product()
+    let x_j = points[j].0;
 
+    for (m, (x_m, _)) in points.iter().enumerate() {
+        if m != j {
+            l_j *= (x - x_m) * (x_j - x_m).invert();
+        }
+    }
+
+    l_j
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use curve25519_dalek_ng::scalar::Scalar;
+    use rand_core::OsRng;
+
+    #[test]
+    fn lagrange_interpolation_test() {
+        let mut csprng = OsRng;
+
+        let rnd = Scalar::random(&mut csprng);
+
+        let point0: (Scalar, Scalar) = (Scalar::zero(), Scalar::zero());
+        let point1: (Scalar, Scalar) = (Scalar::one(), rnd);
+        let two = Scalar::one() + Scalar::one();
+        let point2: (Scalar, Scalar) = (two, two * rnd);
+
+        let mut points = Vec::<(Scalar, Scalar)>::new();
+        points.push(point0);
+        points.push(point1);
+        points.push(point2);
+
+        let ret = lagrange_interpolate(Scalar::one(), &points);
+
+        assert_eq!(ret, rnd);
+    }
 }
