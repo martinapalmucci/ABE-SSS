e61916afe7ed8401ed50a05c7514b3659688fa62
Martina <palmucci997@gmail.com>
2022-07-14 11:40:13 +0200

    Draft 2v2 ecryption/decryption process

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
-use curve25519_dalek_ng::scalar::Scalar;
+use chacha20poly1305::{
+    aead::{Aead, NewAead},
+    ChaCha20Poly1305, Key, Nonce,
+};
+use curve25519_dalek_ng::{
+    constants,
+    ristretto::{CompressedRistretto, RistrettoPoint},
+    scalar::Scalar,
+};
 use rand_core::OsRng;
 
 // SSS algorithm
@@ -66,7 +74,7 @@ fn evaluate_polynomial(polynomial: &[Scalar], x: Scalar) -> Scalar {
 // Recovering secret
 #[must_use]
 pub fn recover_secret(shares: &[(Scalar, Scalar)], threshold: usize) -> Scalar {
-    // check if n_shares >= threshold
+    assert!(shares.len() >= threshold);
 
     lagrange_interpolate(Scalar::zero(), shares)
 }
@@ -94,6 +102,174 @@ fn lagrange_polynomial(j: usize, points: &[(Scalar, Scalar)], x: Scalar) -> Scal
         }
     }
     l_j
+}
+
+/// Returns a keypair. The first element is private key and the second one is public key.
+///
+/// # Arguments
+///
+/// * `csprng` - cryptographically secure pseudorandom number generator
+///
+/// # Example
+///
+/// ```
+/// let mut csprng = OsRng;
+/// let keypair = generate_keypair(&mut csprng);
+/// let (private_key, public_key) = keypair;
+///
+/// let g = constants::RISTRETTO_BASEPOINT_TABLE;
+/// assert_eq!(&private_key * &g, public_key)
+/// ```
+#[must_use]
+pub fn generate_keypair(csprng: &mut OsRng) -> (Scalar, RistrettoPoint) {
+    let private_key = Scalar::random(csprng);
+    let generator = constants::RISTRETTO_BASEPOINT_TABLE;
+    let public_key = &private_key * &generator;
+    (private_key, public_key)
+}
+
+/// Returns the symmetric cipher key for ECIES.
+///
+/// # Arguments
+///
+/// * `pkc` - sender's private key
+/// * `pbk` - receiver's public key
+///
+/// # Example
+///
+/// ```
+/// let mut csprng = OsRng;
+/// let g = constants::RISTRETTO_BASEPOINT_TABLE;
+/// let Alice_pkc = Scalar::random(&mut csprng);
+/// let Alice_pbk = &Alice_pkc * &g;
+/// let Bob_pkc = Scalar::random(&mut csprng);
+/// let Bob_pbk = &Bob_pkc * &g;
+///
+/// let K_A = key_derivation_fn(&Alice_pkc, &Bob_pbk);
+/// let K_B = key_derivation_fn(&Bob_pkc, &Alice_pbk);
+/// assert_eq!(K_A, K_B)
+/// ```
+fn key_derivation_fn(pkc: &Scalar, pbk: &RistrettoPoint) -> [u8; 32] {
+    (pkc * pbk).compress().to_bytes()
+}
+
+/// Generates randomly an ephemeral private key, and its associated public key.
+///
+/// Derives the encryption key and encrypts the plaintext taken as input.
+///
+/// Writes a single message containing both the ciphertext and the public ephemeral value.
+///
+/// # Arguments
+///
+/// * `plaintext` - text to be encrypted
+/// * `pbk` - receiver's public key
+///
+#[must_use]
+pub fn process_encryption(csprng: &mut OsRng, plaintext: &[u8], pbk: &RistrettoPoint) -> Vec<u8> {
+    let ephemeral_keypair = generate_keypair(csprng);
+    let (ephemeral_pkc, ephemeral_pbk) = ephemeral_keypair;
+
+    let cipher_key = key_derivation_fn(&ephemeral_pkc, &pbk);
+    let ciphertext = encrypt(plaintext, &cipher_key);
+
+    write_message(&ciphertext, &ephemeral_pbk)
+}
+
+/// Returns the ciphertext.
+///
+/// # Arguments
+///
+/// * `plaintext` - text to be encrypted
+/// * `cipher_key` - encryption key
+///
+fn encrypt(plaintext: &[u8], cipher_key: &[u8; 32]) -> Vec<u8> {
+    let key = Key::from_slice(cipher_key);
+    let cipher = ChaCha20Poly1305::new(key);
+
+    let ciphertext = cipher
+        .encrypt(&Nonce::default(), plaintext.as_ref())
+        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+
+    ciphertext
+}
+
+/// Returns the concatenation of ciphertext and sender's public key.
+///
+/// # Arguments
+///
+/// * `ciphertext` - encrypted text
+/// * `pbk` - sender's public key
+///
+fn write_message(ciphertext: &Vec<u8>, pbk: &RistrettoPoint) -> Vec<u8> {
+    let mut message: Vec<u8> = Vec::new();
+    message.append(&mut write_ciphertext(ciphertext));
+    message.append(&mut write_pbk(pbk));
+    message
+}
+
+fn write_ciphertext(ciphertext: &Vec<u8>) -> Vec<u8> {
+    ciphertext.clone()
+}
+
+fn write_pbk(pbk: &RistrettoPoint) -> Vec<u8> {
+    pbk.compress().to_bytes().to_vec()
+}
+
+/// Reads the message to get ciphertext and sender's public key.
+///
+/// Derives the decryption key and decrypts the ciphertext taken as input.
+///
+/// Returns the plaintext.
+///
+/// # Arguments
+///
+/// * `message` - received message
+///
+#[must_use]
+pub fn process_decryption(message: &Vec<u8>, pkc: &Scalar) -> Vec<u8> {
+    let (ciphertext, ephemeral_pbk) = read_message(&message);
+    let cipher_key = key_derivation_fn(&pkc, &ephemeral_pbk);
+    decrypt(&ciphertext, &cipher_key)
+}
+
+/// Returns the ciphertext and the sender's public key contained in message.
+///
+/// # Arguments
+///
+/// * `message` - message to be read
+///
+fn read_message(message: &Vec<u8>) -> (Vec<u8>, RistrettoPoint) {
+    let ciphertext = read_ciphertext(message);
+    let pbk = read_pbk(message);
+    (ciphertext, pbk)
+}
+
+fn read_ciphertext(message: &Vec<u8>) -> Vec<u8> {
+    message[0..(message.len() - 32)].to_vec()
+}
+
+fn read_pbk(message: &Vec<u8>) -> RistrettoPoint {
+    let pbk = &message[(message.len() - 32)..message.len()];
+    let pbk = <[u8; 32]>::try_from(pbk).unwrap();
+    CompressedRistretto(pbk).decompress().unwrap()
+}
+
+/// Returns the plaintext.
+///
+/// # Arguments
+///
+/// * `ciphertext` - text to be decrypted
+/// * `cipher_key` - decryption key
+///
+fn decrypt(ciphertext: &[u8], cipher_key: &[u8; 32]) -> Vec<u8> {
+    let key = Key::from_slice(cipher_key);
+    let cipher = ChaCha20Poly1305::new(key);
+
+    let plaintext = cipher
+        .decrypt(&Nonce::default(), ciphertext.as_ref())
+        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+
+    plaintext
 }
 
 #[cfg(test)]
@@ -135,5 +311,25 @@ mod tests {
         let recov_secret = recover_secret(&shares, threshold);
 
         assert_eq!(secret, recov_secret);
+    }
+
+    #[test]
+    fn encryption_decryption_test() {
+        let mut csprng = OsRng;
+
+        let original_text = b"plaintext message";
+        let attribute_keypair = generate_keypair(&mut csprng);
+
+        // Encryption process
+
+        let attribute_pbk = attribute_keypair.1;
+        let message = process_encryption(&mut csprng, original_text, &attribute_pbk);
+
+        // Decryption process
+
+        let attribute_pkc = attribute_keypair.0;
+        let plaintext = process_decryption(&message, &attribute_pkc);
+
+        assert_eq!(plaintext, original_text.to_vec());
     }
 }
